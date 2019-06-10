bfa6657c935471a640cf748292f503ec4a53e88a
Azat Khasanshin <azat.khasanshin@gmail.com>
2019-06-10 16:47:06 +0300

    side bar with list of lines

--- soyla.py
+++ soyla.py
@@ -10,8 +10,13 @@ from scipy.io import wavfile
 class MyEdit(urwid.Edit):
     def keypress(self, size, key):
         if key == 'enter':
-            return
+            return 'enter'
         super(MyEdit, self).keypress(size, key)
+
+
+class MyListBox(urwid.ListBox):
+    def keypress(self, size, key):
+        return key
 
 
 class Soyla(object):
@@ -21,6 +26,10 @@ class Soyla(object):
     EDITING = 3
 
     SAMPLERATE = 44100
+
+    PALETTE = [
+        ('reversed', 'standout', ''),
+    ]
 
     def __init__(self, lines_file, save_dir):
         self.save_dir = save_dir
@@ -50,15 +59,30 @@ class Soyla(object):
         self.line_text = urwid.Text('', align='center')
         self.line_edit = urwid.Edit(align='center')
         self.line = urwid.WidgetPlaceholder(self.line_text)
-        pile = urwid.Pile([
+        self.line_list = MyListBox(urwid.SimpleFocusListWalker(self._get_side_list()))
+        main_window = urwid.Pile([
             ('weight', 1, urwid.Filler(self.line)),
             ('pack', urwid.Divider('-')),
             ('weight', 1, urwid.Filler(self.state_text)),
         ])
-        self.top = pile
+        self.top = urwid.Columns([
+            ('weight', 1, self.line_list),
+            ('weight', 2, main_window),
+        ], dividechars=3)
+
+    def _get_side_list(self):
+        lines = [l[0] for l in self.lines]
+        for i, l in enumerate(lines):
+            lines[i] = "{}. {}".format(i, l)
+        # lines = [l + '...' for l in lines]
+        lines = [urwid.Text(l, wrap='clip') for l in lines]
+        lines = [urwid.AttrMap(l, None, focus_map='reversed') for l in lines]
+        return lines
 
     def handle_input(self, key):
         if key in ('q', 'Q', 'esc'):
+            if self.state == self.EDITING:
+                return self.cancel_edit()
             raise urwid.ExitMainLoop()
         if key in ('r', 'R'):
             return self.record()
@@ -66,9 +90,9 @@ class Soyla(object):
             return self.save()
         if key == ' ':
             return self.play()
-        if key in ('j', 'J'):
+        if key in ('j', 'J', 'down'):
             return self.change_line(1)
-        if key in ('k', 'K'):
+        if key in ('k', 'K', 'up'):
             return self.change_line(-1)
         if key in ('e', 'E'):
             return self.edit()
@@ -121,6 +145,7 @@ class Soyla(object):
             self.l_index = 0
         elif self.l_index >= self.lines_len:
             self.l_index = self.lines_len - 1
+        self.line_list.set_focus(self.l_index)
         self.draw()
 
     def edit(self):
@@ -129,7 +154,15 @@ class Soyla(object):
         self.line.original_widget = self.line_edit
         self.line_edit.set_edit_text(self.cur_line)
         self.line_edit.set_edit_pos(len(self.cur_line))
+        self.top.set_focus(1)
         self.set_state(self.EDITING)
+
+    def cancel_edit(self):
+        if self.state != self.EDITING:
+            return
+        self.line.original_widget = self.line_text
+        self.top.set_focus(0)
+        self.set_state(self.WAITING)
 
     def finish_edit(self):
         if self.state != self.EDITING:
@@ -137,6 +170,7 @@ class Soyla(object):
         self.line.original_widget = self.line_text
         self.cur_line = self.line_edit.get_edit_text()
         self._save_lines()
+        self.top.set_focus(0)
         self.set_state(self.WAITING)
 
     def run(self):
@@ -144,6 +178,7 @@ class Soyla(object):
             self.top,
             unhandled_input=lambda k: self.handle_input(k),
             pop_ups=True,
+            palette=self.PALETTE,
         )
         self.loop.run()
 
@@ -183,8 +218,7 @@ class Soyla(object):
         self.lines[self.l_index] = (self.cur_line, s, True)
 
     def _draw_line_text(self):
-        txt = "%d. %s" % (self.l_index, self.cur_line)
-        self.line_text.set_text(txt)
+        self.line_text.set_text(self.cur_line)
 
 
 def main(input_file, save_dir):
